c7d08406fd08a0f1bc7082b12379202af44509d8
Prosanta Paul <prosantapaulbsmrstu@gmail.com>
2021-01-29 19:44:15 +0600

    input and button

--- src/App.js
+++ src/App.js
@@ -4,20 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <form id="todo-form">
+          <input type="text" placeholder="Enter task" />
+          <button type="submit">Add</button>
+        </form>
       </header>
+      
     </div>
   );
 }
