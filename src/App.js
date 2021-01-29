0c4be492d816d38c73e88a3d094e0ce171bbe036
Prosanta Paul <prosantapaulbsmrstu@gmail.com>
2021-01-29 21:31:37 +0600

    add item

--- src/App.js
+++ src/App.js
@@ -1,18 +1,64 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import ListItems from './ListItems'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
-function App() {
+library.add(faTrash)
+
+
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      items:[],
+      currentItem:{
+        text:'',
+        key:''
+      }
+    }
+    this.addItem = this.addItem.bind(this);
+    this.handleInput = this.handleInput.bind(this);
+  }
+  addItem(e){
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+    if(newItem.text !==""){
+      const items = [...this.state.items, newItem];
+    this.setState({
+      items: items,
+      currentItem:{
+        text:'',
+        key:''
+      }
+    })
+    }
+  }
+  handleInput(e){
+    this.setState({
+      currentItem:{
+        text: e.target.value,
+        key: Date.now()
+      }
+    })
+  }
+  render(){
   return (
     <div className="App">
       <header>
-        <form id="todo-form">
-          <input type="text" placeholder="Enter task" />
+        <form id="todo-form" onSubmit={this.addItem}>
+          <input type="text" placeholder="Enter task" value={this.state.currentItem.text} onChange={this.handleInput}/>
           <button type="submit">Add</button>
         </form>
       </header>
-      
+      <ListItems items={this.state.items} ></ListItems>
     </div>
   );
 }
+}
+
+
 
 export default App;
